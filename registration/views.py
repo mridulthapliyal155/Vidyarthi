5a8c97c514faae34060dd71f7e43a2bde3c1aa6a
Mridul <thapliyamridul19@gmailcom>
2021-01-20 22:49:24 +0530

    updates

--- registration/views.py
+++ registration/views.py
@@ -22,7 +22,7 @@ def register(request):
                 'thankyou for registering in this website, we will not disappoint you with our website.',
                 '21b06project@gmail.com',
                 [email],
-                fail_silently=False,
+                fail_silently=True,
 )
             messages.success(request,'You have successfully registered now please login')
             return redirect('login')
